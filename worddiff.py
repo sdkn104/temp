c82119554178cfeab5cd401b286d4ea14253d2a6
sdkn104 <sdkn104@gmail.com>
2025-02-24 21:19:46 +0900

    update worddiff.py

--- worddiff.py
+++ worddiff.py
@@ -54,8 +54,9 @@ def extract_changed_paragraphs(docx_path):
         if has_ins or has_del:
             before_text = get_paragraph_text_lxml(p, mode='before')
             after_text = get_paragraph_text_lxml(p, mode='after')
-            results.append((before_text, after_text))
-            print("xxxxx")
+            if before_text != after_text:
+              results.append((before_text, after_text))
+              print("xxxxx")
     return results
 
 if __name__ == '__main__':
